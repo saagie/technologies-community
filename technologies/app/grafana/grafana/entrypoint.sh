c3e7ded5963e98951ee425dd60fa05b9d174df67
courbeb <baptiste.courbe@saagie.com>
2021-01-11 14:55:01 +0100

    create temp release

--- technologies/app/grafana/grafana/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo $SAAGIE_BASE_PATH
-sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/grafana/grafana.ini
-sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/nginx/sites-enabled/grafana.conf
-nginx && /run.sh
