d61e8bef74a7176fc582671bc7e2392f916bb7be
Thomas Quiviger <thomas.quiviger@saagie.com>
2021-01-15 09:41:07 +0100

    Refactoring

--- app/shiny-proxy/shiny-proxy-2.4.3/run-app.sh
+++ app/shiny-proxy/shiny-proxy-2.4.3/run-app.sh
@@ -2,11 +2,14 @@
 
 case "$SHINYPROXY_CONF_URL" in 
     s3*) 
+        echo "[INFO] - Fetching application.yml file from S3"
         aws s3 cp $SHINYPROXY_CONF_URL /opt/shinyproxy/application.yml;;
     http*)
+        echo "[INFO] - Fetching application.yml file"
         wget $SHINYPROXY_CONF_URL -O /opt/shinyproxy/application.yml;;
     *)
-        echo "Configuration file cannot be fetched, protocol not compatible, choose between s3 or http"
+        echo "[WARN] - application.yml configuration file cannot be fetched : protocol not compatible => choose between s3 or http"
+        echo "[INFO] - Sample application.yml file will be used"
 esac
 
 sed -i 's#SAAGIE_BASE_PATH#'"$SAAGIE_BASE_PATH"'#g' /opt/shinyproxy/application.yml
