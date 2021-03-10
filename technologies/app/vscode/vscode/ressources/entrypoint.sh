05a1e1c1b55fee8c6b1bc02f628a7ab53d10ecc6
Augustin Peyridieux <augustin.peyridieux@saagie.com>
2021-03-10 11:04:29 +0100

    push working image + working local image

--- technologies/app/vscode/vscode/ressources/entrypoint.sh
+++ technologies/app/vscode/vscode/ressources/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+echo "SAAGIE_BASE_PATH"
+echo $SAAGIE_BASE_PATH
+
+# /init
+
 sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/nginx/sites-enabled/nginx.conf
-nginx && /init_vscode.sh
+nginx && /init
