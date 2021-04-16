4f4d4e2c95a0aace1d512848b41bbca80b41c657
Thomas Quiviger <tquiviger@gmail.com>
2021-04-16 14:34:30 +0200

    Issue #50 : upgrading Grafana to 7.5.4 (#51)
    * Issue #50 : upgrading Grafana to 7.5.4 and forcing admin password to be set with env variable
    
    Co-authored-by: Baptiste Courbe <baptiste.courbe@saagie.com>
    Co-authored-by: Baptiste Courbe <babacourbe@gmail.com>

--- technologies/app/grafana/grafana-7.3.4/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/grafana/grafana.ini
-sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/nginx/sites-enabled/grafana.conf
-nginx && /run.sh
