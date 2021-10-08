c4f3859b730aa9dd4a605cf7537243c6df2337a5
Julien Fricou <julien.fricou@saagie.com>
2021-10-08 14:22:25 +0200

    WIP create postgres database at launch

---
+++ technologies/app/it_supervision/README.md
@@ -0,0 +1,8 @@
+Les variables d'environnement à créer sur saagie :
+    SAAGIE_SUPERVISION_LOGIN
+    SAAGIE_SUPERVISION_PASSWORD
+    SAAGIE_URL
+    SAAGIE_REALM
+    SAAGIE_PLATFORM_ID
+
+Le user applicatif saagie devra avoir au moins le droit viewer sur tous les projets
--- technologies/app/it_supervision/it_supervision/it_supervision/entrypoint.sh
+++ technologies/app/it_supervision/it_supervision/it_supervision/entrypoint.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+su postgres -c 'source /etc/profile && pg_ctl start -D /var/lib/postgresql/data'
+su postgres -c 'psql --command "CREATE USER supervision_pg_user"'
+su postgres -c 'psql --command "CREATE DATABASE supervision_pg_db"'
+su postgres -c 'psql --command "GRANT ALL PRIVILEGES ON DATABASE supervision_pg_db to supervision_pg_user"'
+su postgres -c 'psql -U supervision_pg_user --command $(cat infra.sql)'
+
 
 if [[ -z "${GRAFANA_ADMIN_PASSWORD}" ]]; then
   echo "ERROR : Grafana admin password must be set through GRAFANA_ADMIN_PASSWORD environment variable. Exiting."
@@ -7,8 +14,7 @@ else
   sed -i 's:GRAFANA_ADMIN_PASSWORD:'"$GRAFANA_ADMIN_PASSWORD"':g' /etc/grafana/grafana.ini
 fi
 
-pg_ctl start -D /var/lib/postgresql/data
-
 sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/grafana/grafana.ini
 sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/nginx/sites-enabled/grafana.conf
+
 nginx && /run.sh
