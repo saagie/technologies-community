29cb59034db9b2b6ce537d8e0fa507dbb14d0479
Augustin Peyridieux <augustin.peyridieux@saagie.com>
2022-01-07 12:02:54 +0100

    fix listener port + add readme + add entrypoint.sh

--- technologies/app/rshiny/rshiny-1.7.1/ressources/entrypoint.sh
+++ technologies/app/rshiny/rshiny-1.7.1/ressources/entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -xeuo pipefail
 
-echo "SAAGIE_BASE_PATH = SAAGIE_BASE_PATH"
+echo "SAAGIE_BASE_PATH = $SAAGIE_BASE_PATH"
+SHINY_GIT_BRANCH=${SHINY_GIT_BRANCH:-master}
 echo "Setting SHINY_GIT_BRANCH to $SHINY_GIT_BRANCH..."
-SHINY_GIT_BRANCH=${DASH_GIT_BRANCH:-master}
-echo "cloning repo from DASH_GIT_URL_REPOSITORY: $DASH_GIT_URL_REPOSITORY"
+echo "cloning repo from SHINY_GIT_URL_REPOSITORY: $SHINY_GIT_URL_REPOSITORY"
 
 
-git clone $DASH_GIT_URL_REPOSITORY --branch $SHINY_GIT_BRANCH --single-branch --depth 1 app
+git clone $SHINY_GIT_URL_REPOSITORY --branch $SHINY_GIT_BRANCH --single-branch --depth 1 app
 
-sudo sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/nginx/sites-enabled/shiny.conf
-nginx && Rscript -e 'library(shiny); runApp("app", 3838)'
+sudo sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/nginx/sites-enabled/nginx.conf
+nginx && Rscript -e 'library(methods); library(shiny); runApp("app", 3838)'
 
