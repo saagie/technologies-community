dabab2f134e28f70018e73b5965d329551b519ab
Augustin Peyridieux <augustin.peyridieux@saagie.com>
2022-01-07 15:40:57 +0100

    fix missing letter

--- technologies/app/rshiny/rshiny-1.7.1/ressources/entrypoint.sh
+++ technologies/app/rshiny/rshiny-1.7.1/ressources/entrypoint.sh
@@ -3,7 +3,7 @@
 set -xeuo pipefail
 
 echo "
-- 'SHINY_GIT_URL_REPOSITORY' variable is mandatory and should be set to the name of the git repository continning your RShiny app source code (e.g.:https://github.com/saagie/demo-r-shiny.git).
+- 'SHINY_GIT_URL_REPOSITORY' variable is mandatory and should be set to the name of the git repository containning your RShiny app source code (e.g.:https://github.com/saagie/demo-r-shiny.git).
 - 'SHINY_GIT_BRANCH' is optional and defaulted to master, it indicates a specific branch to checkout.
 "
 
