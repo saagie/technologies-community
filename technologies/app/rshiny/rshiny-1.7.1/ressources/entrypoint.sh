954c25ae37405abcbc133b629ebe28a1adc8d08b
Augustin Peyridieux <augustin.peyridieux@saagie.com>
2022-01-07 14:46:51 +0100

    add message in entrypoiunt.sh to help users

--- technologies/app/rshiny/rshiny-1.7.1/ressources/entrypoint.sh
+++ technologies/app/rshiny/rshiny-1.7.1/ressources/entrypoint.sh
@@ -2,11 +2,21 @@
 
 set -xeuo pipefail
 
+echo "
+- 'SHINY_GIT_URL_REPOSITORY' variable is mandatory and should be set to the name of the git repository continning your RShiny app source code (e.g.:https://github.com/saagie/demo-r-shiny.git).
+- 'SHINY_GIT_BRANCH' is optional and defaulted to master, it indicates a specific branch to checkout.
+"
+
+if [[ -z "${SHINY_GIT_URL_REPOSITORY}" ]]; then
+  echo "ERROR : Variable SHINY_GIT_URL_REPOSITORY must be set before running the app. 
+  See https://docs.saagie.io/user/latest/tutorials/projects-module/projects/envar/index.html#projects-create-envar-project for more information"
+  exit 1
+fi
+
 echo "SAAGIE_BASE_PATH = $SAAGIE_BASE_PATH"
 SHINY_GIT_BRANCH=${SHINY_GIT_BRANCH:-master}
-echo "Setting SHINY_GIT_BRANCH to $SHINY_GIT_BRANCH..."
+echo "Setting SHINY_GIT_BRANCH to $SHINY_GIT_BRANCH"
 echo "cloning repo from SHINY_GIT_URL_REPOSITORY: $SHINY_GIT_URL_REPOSITORY"
-
 
 git clone $SHINY_GIT_URL_REPOSITORY --branch $SHINY_GIT_BRANCH --single-branch --depth 1 app
 
