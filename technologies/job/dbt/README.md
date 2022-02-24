8ff57ad666379c962bf4e1b4c47d69cf2b96c403
Thomas Quiviger <thomas.quiviger@saagie.com>
2022-02-24 16:08:14 +0100

    Adding dbt

---
+++ technologies/job/dbt/README.md
@@ -0,0 +1,11 @@
+# DBT
+
+
+![Docker Image Size (tag)](https://img.shields.io/docker/image-size/saagie/spark/3.2-aws-jre-11-0.36?label=v3.2-aws-jre-11%20image%20size&style=for-the-badge)
+
+## Description
+
+## How to build in local
+
+
+
---
+++ technologies/job/dbt/dbt/resources/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if compgen -G "*.zip*" > /dev/null; then
+    unzip -q *.zip
+elif
+    echo "ERROR : You must submit a zip file containing your dbt project"
+fi
+
+if test -f main_script;
+then sh ./main_script;
+else exec "$@"
+fi;
