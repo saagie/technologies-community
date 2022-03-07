39f195c6c8530af710cd2d764094eced0e36e7f7
Nicolas Chmiel <nicolas@creativedata.fr>
2022-03-07 15:47:38 +0100

    change entrypoint.sh after review

--- technologies/app/pgadmin4/pgadmin_6.5/entrypoint_saagie.sh
+++ technologies/app/pgadmin4/pgadmin_6.5/entrypoint_saagie.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-if [[ -z "${SAAGIE_BASE_PATH}" ]]; then
-  echo 'ERROR : $SAAGIE_BASE_PATH variable must be set before launch. Exiting.'
+if [[ -z "${PGADMIN_DEFAULT_EMAIL}" ]]; then
+  echo 'ERROR : $PGADMIN_DEFAULT_EMAIL variable must be set before launch. Exiting.'
   exit 1
+fi
+
+if [[ -z "${PGADMIN_DEFAULT_PASSWORD}" ]]; then
+  echo 'ERROR : $PGADMIN_DEFAULT_PASSWORD variable must be set before launch. Exiting.'
+  exit 2
 fi
 
 echo "launching Nginx"
 sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/nginx/http.d/pgadmin.conf
-#nginx -g 'pid /tmp/nginx.pid; daemon on;'
 
-
-cat /etc/nginx/http.d/pgadmin.conf
 nginx -g 'pid /tmp/nginx.pid; daemon on;' && /entrypoint.sh
 
