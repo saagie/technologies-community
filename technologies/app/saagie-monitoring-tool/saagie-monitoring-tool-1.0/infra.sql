8c428fd0726a6f4310609b0e39261b0f6ef1592f
Qiwei <60429863+Qiwei08@users.noreply.github.com>
2022-10-21 14:06:25 +0200

    bug/saagie_monitoring_tool_with_Saagie3
    * add new version for SMT
    
    * Feature/hadoop vscode (#164)
    
    * Hadoop to vscode python
    
    * [BOT] Set version for this branch
    
    Co-authored-by: Nicolas <nicolas@saagie.com>
    Co-authored-by: Team Service <service@saagie.com>
    
    * [BOT] Set new version
    
    * update metadata.yaml
    
    * [BOT] Set version for this branch
    
    * add new version for SMT
    
    * update metadata.yaml
    
    * [BOT] Set version for this branch
    
    Co-authored-by: NicolasCD <nicolas@creativedata.fr>
    Co-authored-by: Nicolas <nicolas@saagie.com>
    Co-authored-by: Team Service <service@saagie.com>

---
+++ technologies/app/saagie-monitoring-tool/saagie-monitoring-tool-1.0/infra.sql
@@ -0,0 +1,62 @@
+create TABLE supervision_saagie
+  (
+     supervision_timestamp TIMESTAMP,
+     project_id            VARCHAR(60),
+     project_name          VARCHAR(200),
+     orchestration_type    VARCHAR(10),
+     orchestration_id      VARCHAR(60),
+     orchestration_name    VARCHAR(200),
+     instance_id           VARCHAR(60),
+     instance_start_time   TIMESTAMP,
+     instance_end_time     TIMESTAMP,
+     instance_status       VARCHAR(30),
+     instance_duration     BIGINT,
+     instance_saagie_url   VARCHAR(200),
+     PRIMARY KEY (project_id,orchestration_id, instance_id)
+  );
+
+create TABLE supervision_saagie_jobs
+  (
+     project_id             VARCHAR(60),
+     project_name           VARCHAR(200),
+     creation_date          TIMESTAMP,
+     orchestration_type     VARCHAR(10),
+     orchestration_category VARCHAR(60),
+     orchestration_id       VARCHAR(60),
+     orchestration_name     VARCHAR(200),
+     instance_count         INT,
+     technology             VARCHAR(60),
+     PRIMARY KEY (project_id, orchestration_id)
+  );
+
+create TABLE supervision_saagie_apps
+  (
+     project_id             VARCHAR(60),
+     project_name           VARCHAR(200),
+     orchestration_type     VARCHAR(10),
+     orchestration_id       VARCHAR(60),
+     orchestration_name     VARCHAR(200),
+     creation_date          TIMESTAMP,
+     current_status         VARCHAR(20),
+     start_time             TIMESTAMP,
+     stop_time              TIMESTAMP,
+     technology             VARCHAR(60),
+     PRIMARY KEY (project_id, orchestration_id)
+  );
+
+create TABLE supervision_saagie_jobs_snapshot
+  (
+     project_id    VARCHAR(60),
+     project_name  VARCHAR(200),
+     snapshot_date DATE,
+     job_count     INT,
+     PRIMARY KEY (snapshot_date, project_id)
+  );
+
+create TABLE supervision_datalake
+  (
+     supervision_date  DATE,
+     supervision_label VARCHAR(60),
+     supervision_value NUMERIC(20, 2),
+     PRIMARY KEY (supervision_date, supervision_label)
+  );
