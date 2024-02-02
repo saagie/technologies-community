ae15884190896a3b2c3a218c984eea0b14ab95fc
Guillaume Prevost <guillaume.prevost@saagie.com>
2024-02-02 15:59:24 +0100

    Add Streamlit

---
+++ technologies/app/streamlit/1.31.0/resources/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+STREAMLIT_GIT_BRANCH=${STREAMLIT_GIT_BRANCH:-master}
+echo "Setting STREAMLIT_GIT_BRANCH to $STREAMLIT_GIT_BRANCH..."
+
+git clone $STREAMLIT_GIT_URL_REPOSITORY --branch $STREAMLIT_GIT_BRANCH --single-branch --depth 1 app
+
+cd app
+
+pip install -r ./requirements.txt
+
+export STREAMLIT_URL_BASE_PATHNAME=${SAAGIE_BASE_PATH}"/"
+
+python -u ./app.py
